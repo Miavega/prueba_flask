ee86c5a86d85d6b599b7bf76d0a2947f85b82edb
Miavega <miguel_9504@hotmail.com>
2019-05-30 00:05:32 -0500

    Se añaden consultas a equipos

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+from models.models import *
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,80 +9,97 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from models.models import *
 
 app.secret_key = 'clave-secreta'
 
+
 @app.route('/')
 def Index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/equipo')
 def equipo():
-	return render_template('equipo.html', equipos = consultar_equipo())
+	consulta = Equipo.query.all()
+	return render_template('equipo.html', equipos=consulta)
+
 
 @app.route('/crear_usuario', methods=['POST'])
 def crear_usuario():
-	nombre = request.args.get('nombre')
-	email=request.args.get('email')
-	equipo=request.args.get('equipo')
-	try:
-		usuario=Usuario(
-			nombre=nombre,
+    nombre = request.args.get('nombre')
+    email = request.args.get('email')
+    equipo = request.args.get('equipo')
+    try:
+        usuario = Usuario(
+            nombre=nombre,
             email=email,
             equipo=equipo
         )
-		db.session.add(usuario)
+        db.session.add(usuario)
+        db.session.commit()
+        flash("Usuario creado. usuario id={}".format(usuario.id))
+        return redirect(url_for('equipo'))
+    except Exception as e:
+        return(str(e))
+
+
+@app.route('/modificar_usuario')
+def modificar_usuario():
+    return 'asdsa'
+
+
+@app.route('/borrar_usuario')
+def borrar_usuario():
+    return 'asdsa'
+
+
+@app.route('/consultar_usuario')
+def consultar_usuario():
+    return 'asdsa'
+
+
+@app.route('/crear_equipo', methods=['POST'])
+def crear_equipo():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        desc_equipo = request.form['desc_equipo']
+        try:
+            equipo = Equipo(
+                nombre=nombre,
+                desc_equipo=desc_equipo,
+            )
+            db.session.add(equipo)
+            db.session.commit()
+            flash("Equipo creado. equipo id={}".format(equipo.id))
+            return redirect(url_for('equipo'))
+        except Exception as e:
+            return(str(e))
+
+
+@app.route('/modificar_equipo')
+def modificar_equipo():
+    return 'asdsa'
+
+
+@app.route('/borrar_equipo/<string:id>')
+def borrar_equipo(id):
+	try:
+		equipo = Equipo.query.filter_by(id=id).first()
+		db.session.delete(equipo)
 		db.session.commit()
-		flash("Usuario creado. usuario id={}".format(usuario.id))
 		return redirect(url_for('equipo'))
 	except Exception as e:
 		return(str(e))
 
-@app.route('/modificar_usuario')
-def modificar_usuario():
-	return 'asdsa'
-
-@app.route('/borrar_usuario')
-def borrar_usuario():
-	return 'asdsa'
-
-@app.route('/consultar_usuario')
-def consultar_usuario():
-	return 'asdsa'
-
-@app.route('/crear_equipo', methods=['POST'])
-def crear_equipo():
-	if request.method == 'POST':
-		nombre = request.form['nombre']
-		desc_equipo = request.form['desc_equipo']
-		try:
-			equipo=Equipo(
-				nombre=nombre,
-				desc_equipo=desc_equipo,
-			)
-			db.session.add(equipo)
-			db.session.commit()
-			flash("Equipo creado. equipo id={}".format(equipo.id))
-			return redirect(url_for('equipo'))
-		except Exception as e:
-			return(str(e))
-
-@app.route('/modificar_equipo')
-def modificar_equipo():
-	return 'asdsa'
-
-@app.route('/borrar_equipo')
-def borrar_equipo():
-	return 'asdsa'
 
 @app.route('/consultar_equipo', methods=['GET'])
 def consultar_equipo():
-	try:
-		equipos=Equipo.query.all()
-		return  jsonify([equipo.serialize() for equipo in equipos])
-	except Exception as e:
-		return(str(e))
+    try:
+        equipos = Equipo.query.all()
+        return jsonify([equipo.serialize() for equipo in equipos])
+    except Exception as e:
+        return(str(e))
+
 
 if __name__ == '__main__':
-	app.run(port = 3000, debug = True)
+    app.run(port=3000, debug=True)
